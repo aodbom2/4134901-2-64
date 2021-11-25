cdbca36533bbe6093c3f4796b301f844c4fce135
aodbom2 <59988070+aodbom2@users.noreply.github.com>
2021-11-25 14:29:10 +0700

    commit

---
+++ LAB3/hello-react/src/components/Name.js
@@ -0,0 +1,10 @@
+function MyName(){
+    return(
+        <div className="MyName">
+            <div>นายปุณณภพ แจ่มประโคน</div>
+            <div>610112418016</div>
+        </div>
+    );
+}
+
+export default MyName;
